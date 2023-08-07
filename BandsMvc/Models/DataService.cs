6e7af4b356061c0867ca267bf99deae2ad5178fe
Andreas Andersson <Andreas__Andersson@hotmail.se>
2023-08-07 16:10:29 +0200

    del 2

--- BandsMvc/Models/DataService.cs
+++ BandsMvc/Models/DataService.cs
@@ -2,11 +2,25 @@
 {
     public class DataService
     {
-      
-        List<Product> bands = new List<Product> 
+        List<Band> bands = new List<Band>
         {
+
+         new Band {Id = 1, Name = "Post Malone", Description = "Hip Hop"},
+         new Band {Id = 2, Name = "Hooja", Description = "Pop"},
+         new Band {Id = 3, Name = "Lous and the Yakuza", Description = "R&B"},
+         new Band {Id = 4, Name = "Wu-Tang Clan", Description = "Hip Hop"},
+         new Band {Id = 5, Name = "Army Of The Pharaohs", Description = "Hip Hop"},
 
         };
 
+        public Band GetBandById(int id)
+        {
+            return bands.SingleOrDefault(band => band.Id == id);
+                
+        }
+        public Band[] GetAllBands()
+        {
+            return bands.ToArray();
+        }
     }
 }
